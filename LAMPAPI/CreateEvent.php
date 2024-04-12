7f9fd0f1b424ac84a31f0e3a3f30977e3598ce40
Delali Ekpeh <delali1332@hotmail.com>
2024-04-11 22:32:00 -0400

    Create CreateEvent.php

---
+++ LAMPAPI/CreateEvent.php
@@ -0,0 +1,51 @@
+<?php
+    $inData = getRequestInfo();
+    $time = $inData["time"];
+    $location = $inData["location"];
+    $eventName = $inData["eventName"];
+    $description = $inData["description"];
+    $eventType = $inData["eventType"];
+
+    // Create database connection
+    $conn = new mysqli("localhost", "TheApi", "1verygoodPassword", "COP4331");
+
+    // Check connection
+    if ($conn->connect_error)
+    {
+        returnWithError($conn->connect_error);
+    }
+    else
+    {
+        $stmt = $conn->prepare("INSERT INTO Events (Time, Location, Event_name, Description, EventType) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssss", $time, $location, $eventName, $description, $eventType);
+
+        if ($stmt->execute()) {
+            // Get the ID of the last inserted row
+            $last_id = $conn->insert_id;
+            // Return the ID as JSON response
+            sendResultInfoAsJson(json_encode(["message" => "New Event Record was created", "id" => $last_id]));
+        } else {
+            returnWithError($conn->error);
+        }
+
+        $stmt->close();
+        $conn->close();
+    }
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson($obj)
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError($err)
+    {
+        $retValue = '{"error":"' . $err . '"}';
+        sendResultInfoAsJson($retValue);
+    }
+?>
