484305f4050cfe73d29b2e3c67ddeeafeb2daa87
Delali Ekpeh <delali1332@hotmail.com>
2024-04-12 13:48:49 -0400

    Update CreateEvent.php

--- LAMPAPI/CreateEvent.php
+++ LAMPAPI/CreateEvent.php
@@ -5,9 +5,14 @@
     $eventName = $inData["eventName"];
     $description = $inData["description"];
     $eventType = $inData["eventType"];
+    $eventDay = $inData["eventDay"];
+    $eventTime = $inData["eventTime"];
+    $superAdminID = $inData["superAdminID"];
+    $locationAddress = $inData["locationAddress"];
+
 
     // Create database connection
-     $conn = new mysqli("localhost", "Admin", "password", "EventPlannerDB");
+    $conn = new mysqli("localhost", "Admin", "password", "EventPlannerDB");
 
     // Check connection
     if ($conn->connect_error)
@@ -16,27 +21,47 @@
     }
     else
     {
-        // Check if the user has Admin privileges
-        $stmt = $conn->prepare("SELECT Privileges FROM Users WHERE Username = ?");
+        // Check if the user is an Admin
+        $stmt = $conn->prepare("SELECT UID, Privileges FROM Users WHERE Username = ?");
         $stmt->bind_param("s", $username);
         $stmt->execute();
         $result = $stmt->get_result();
 
-        // If user has Admin privileges, proceed with event creation
-        if ($result->num_rows > 0 && $result->fetch_assoc()["Privileges"] === "Admin") {
-            $stmt = $conn->prepare("INSERT INTO Events (Location, Event_name, Description, EventType) VALUES (?, ?, ?, ?)");
-            $stmt->bind_param("ssss", $location, $eventName, $description, $eventType);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $userID = $row["UID"];
+            $userPrivileges = $row["Privileges"];
 
-            if ($stmt->execute()) {
-                // Get the ID of the last inserted row
-                $last_id = $conn->insert_id;
-                // Return the ID as JSON response
-                sendResultInfoAsJson(json_encode(["message" => "New Event Record was created", "id" => $last_id]));
+            // Check if the user is an Admin
+            if ($userPrivileges === "Admin") {
+                // Check if the SuperAdmin ID is correct
+                $stmt = $conn->prepare("SELECT UID FROM Users WHERE UID = ? AND Privileges = 'SuperAdmin'");
+                $stmt->bind_param("i", $superAdminID);
+                $stmt->execute();
+                $result = $stmt->get_result();
+
+                if ($result->num_rows > 0) {
+                    // SuperAdmin ID is correct, proceed with event creation
+                    $stmt = $conn->prepare("INSERT INTO Events (Location, Location_Address, Event_name, Description, EventType, Event_Day, Event_Time) VALUES (?, ?, ?, ?, ?, ?, ?)");
+                    $stmt->bind_param("sssssss", $location, $locationAddress, $eventName, $description, $eventType, $eventDay, $eventTime);
+
+
+                    if ($stmt->execute()) {
+                        // Get the ID of the last inserted row
+                        $last_id = $conn->insert_id;
+                        // Return the ID as JSON response
+                        sendResultInfoAsJson(json_encode(["message" => "New Event Record was created", "id" => $last_id]));
+                    } else {
+                        returnWithError($conn->error);
+                    }
+                } else {
+                    returnWithError("Incorrect SuperAdmin ID.");
+                }
             } else {
-                returnWithError($conn->error);
+                returnWithError("User does not have sufficient privileges to create an event.");
             }
         } else {
-            returnWithError("User does not have sufficient privileges to create an event.");
+            returnWithError("User not found.");
         }
 
         $stmt->close();
