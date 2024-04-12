975d9e1334acefdeed51d6f6d782634d4e6d3bec
Delali Ekpeh <delali1332@hotmail.com>
2024-04-12 01:15:15 -0400

    Update CreateEvent.php

--- LAMPAPI/CreateEvent.php
+++ LAMPAPI/CreateEvent.php
@@ -1,12 +1,13 @@
 <?php
     $inData = getRequestInfo();
+    $username = $inData["username"];
     $location = $inData["location"];
     $eventName = $inData["eventName"];
     $description = $inData["description"];
     $eventType = $inData["eventType"];
 
     // Create database connection
-    $conn = new mysqli("localhost", "Admin", "password", "EventPlannerDB");
+     $conn = new mysqli("localhost", "Admin", "password", "EventPlannerDB");
 
     // Check connection
     if ($conn->connect_error)
@@ -15,16 +16,27 @@
     }
     else
     {
-        $stmt = $conn->prepare("INSERT INTO Events (Location, Event_name, Description, EventType) VALUES (?, ?, ?, ?)");
-        $stmt->bind_param("ssss", $location, $eventName, $description, $eventType);
+        // Check if the user has Admin privileges
+        $stmt = $conn->prepare("SELECT Privileges FROM Users WHERE Username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-        if ($stmt->execute()) {
-            // Get the ID of the last inserted row
-            $last_id = $conn->insert_id;
-            // Return the ID as JSON response
-            sendResultInfoAsJson(json_encode(["message" => "New Event Record was created", "id" => $last_id]));
+        // If user has Admin privileges, proceed with event creation
+        if ($result->num_rows > 0 && $result->fetch_assoc()["Privileges"] === "Admin") {
+            $stmt = $conn->prepare("INSERT INTO Events (Location, Event_name, Description, EventType) VALUES (?, ?, ?, ?)");
+            $stmt->bind_param("ssss", $location, $eventName, $description, $eventType);
+
+            if ($stmt->execute()) {
+                // Get the ID of the last inserted row
+                $last_id = $conn->insert_id;
+                // Return the ID as JSON response
+                sendResultInfoAsJson(json_encode(["message" => "New Event Record was created", "id" => $last_id]));
+            } else {
+                returnWithError($conn->error);
+            }
         } else {
-            returnWithError($conn->error);
+            returnWithError("User does not have sufficient privileges to create an event.");
         }
 
         $stmt->close();
