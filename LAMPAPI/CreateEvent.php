b0962fd69a829cc7d9751df7296d73e7a1e06a53
Delali Ekpeh <delali1332@hotmail.com>
2024-04-13 22:47:33 -0400

    Update CreateEvent.php

--- LAMPAPI/CreateEvent.php
+++ LAMPAPI/CreateEvent.php
@@ -41,42 +41,54 @@
 
                 if ($result->num_rows > 0) {
                     // SuperAdmin ID is correct, proceed with event creation
-                    $stmt = $conn->prepare("INSERT INTO Events (Location, Location_Address, Event_name, Description, EventType, Event_Day, Event_Time) VALUES (?, ?, ?, ?, ?, ?, ?)");
-                    $stmt->bind_param("sssssss", $location, $locationAddress, $eventName, $description, $eventType, $eventDay, $eventTime);
+                    // Check if there's already an event at the same location and time
+                    $stmt = $conn->prepare("SELECT * FROM Events WHERE Location = ? AND Event_Day = ? AND Event_Time = ?");
+                    $stmt->bind_param("sss", $location, $eventDay, $eventTime);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
 
-                    if ($stmt->execute()) {
-                        // Get the ID of the last inserted row
-                        $last_id = $conn->insert_id;
-                        
-                        // Insert event into appropriate table based on event type
-                        switch ($eventType) {
-                            case "RSO":
-                                $stmt2 = $conn->prepare("INSERT INTO RSO_Events (RSO_ID, Events_ID) VALUES (?, ?)");
-                                $stmt2->bind_param("ii", $userID, $last_id);
-                                break;
-                            case "Public":
-                                $stmt2 = $conn->prepare("INSERT INTO Public_Events (Event_Name, Description, Location) VALUES (?, ?, ?)");
-                                $stmt2->bind_param("sss", $eventName, $description, $location);
-                                break;
-                            case "Private":
-                                $stmt2 = $conn->prepare("INSERT INTO Private_Events (Event_Name, Description, Location) VALUES (?, ?, ?)");
-                                $stmt2->bind_param("sss", $eventName, $description, $location);
-                                break;
-                            default:
-                                // If the event type is none of the above, insert into Events table
-                                $stmt2 = $conn->prepare("INSERT INTO Events (Location_Address, Event_name, Description, EventType, Event_Day, Event_Time) VALUES (?, ?, ?, ?, ?, ?)");
-                                $stmt2->bind_param("ssssss", $locationAddress, $eventName, $description, $eventType, $eventDay, $eventTime);
-                        }
-                        
-                        // Bind parameters and execute the statement
-                        if (isset($stmt2)) {
-                            $stmt2->execute();
-                        }
-                        
-                        // Return the ID as JSON response
-                        sendResultInfoAsJson(json_encode(["message" => "New Event Record was created", "id" => $last_id]));
+                    if ($result->num_rows > 0) {
+                        // An event already exists at the same location and time
+                        returnWithError("An event already exists at the same location and time.");
                     } else {
-                        returnWithError($conn->error);
+                        // No conflicting event found, proceed with event creation
+                        $stmt = $conn->prepare("INSERT INTO Events (Location, Location_Address, Event_name, Description, EventType, Event_Day, Event_Time) VALUES (?, ?, ?, ?, ?, ?, ?)");
+                        $stmt->bind_param("sssssss", $location, $locationAddress, $eventName, $description, $eventType, $eventDay, $eventTime);
+
+                        if ($stmt->execute()) {
+                            // Get the ID of the last inserted row
+                            $last_id = $conn->insert_id;
+                            
+                            // Insert event into appropriate table based on event type
+                            switch ($eventType) {
+                                case "RSO":
+                                    $stmt2 = $conn->prepare("INSERT INTO RSO_Events (RSO_ID, Events_ID) VALUES (?, ?)");
+                                    $stmt2->bind_param("ii", $userID, $last_id);
+                                    break;
+                                case "Public":
+                                    $stmt2 = $conn->prepare("INSERT INTO Public_Events (Event_Name, Description, Location) VALUES (?, ?, ?)");
+                                    $stmt2->bind_param("sss", $eventName, $description, $location);
+                                    break;
+                                case "Private":
+                                    $stmt2 = $conn->prepare("INSERT INTO Private_Events (Event_Name, Description, Location) VALUES (?, ?, ?)");
+                                    $stmt2->bind_param("sss", $eventName, $description, $location);
+                                    break;
+                                default:
+                                    // If the event type is none of the above, insert into Events table
+                                    $stmt2 = $conn->prepare("INSERT INTO Events (Location_Address, Event_name, Description, EventType, Event_Day, Event_Time) VALUES (?, ?, ?, ?, ?, ?)");
+                                    $stmt2->bind_param("ssssss", $locationAddress, $eventName, $description, $eventType, $eventDay, $eventTime);
+                            }
+                            
+                            // Bind parameters and execute the statement
+                            if (isset($stmt2)) {
+                                $stmt2->execute();
+                            }
+                            
+                            // Return the ID as JSON response
+                            sendResultInfoAsJson(json_encode(["message" => "New Event Record was created", "id" => $last_id]));
+                        } else {
+                            returnWithError($conn->error);
+                        }
                     }
                 } else {
                     returnWithError("Incorrect SuperAdmin ID.");
